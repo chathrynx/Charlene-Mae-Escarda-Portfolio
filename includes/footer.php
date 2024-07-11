21342e3f0d00b016c9980955c72d16038a90b915
Charlene Mae Escarda <90608146+sharihhh@users.noreply.github.com>
2024-07-11 22:48:15 +0800

    First Commit
    Test: July 11, 2024: 10:47PM

---
+++ includes/footer.php
@@ -0,0 +1,43 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+ 
+ <!-- Footer Section-->
+ <footer class="footer_container">
+  <div class="footer_left">
+      
+      <p class="footer_links">
+            <a href="#">HOME</a> | <a href="#">ABOUT</a> | <a href="#">PORTFOLIO</a> | <a href="#">SERVICE</a> | <a href="#">CONTACT</a>
+      </p>
+      <p class="footer_company_name">Copyright © 2024 <strong>Charlene Mae Escarda</strong> All rights reserved</p>
+  </div>
+
+  <div class="footer_center">
+        <div>
+          <i class="fa fa-map-marker"></i>
+          <p>Mabolo, <span>Cebu City</span>, Philippines</p>
+        </div>
+
+        <div>
+          <i class="fa fa-phone"></i>
+          <p><a href="telno:09453811818">+63-945-381-1818</a></p>
+        </div>
+        <div>
+          <i class="fa fa-envelope"></i>
+          <p><a href="mailto:charlene.escarda@gmail.com">charlene.escarda@gmail.com</a></p>
+        </div>
+  </div>
+
+  <div class="footer_right">
+    <div class="footer_icons">
+        <a href="https://www.linkedin.com/in/charlene-mae-e-a30602247/" target="_blank"><i class="fa fa-linkedin"></i></a>
+        <a href="https://www.facebook.com/tsarlean?mibextid=LQQJ4d" target="_blank"><i class="fa fa-facebook"></i></a>
+        <a href="https://www.instagram.com/chathrynx/" target="_blank"><i class="fa fa-instagram"></i></a> 
+    </div>
+  </div>
+</footer>
+
+    <script src = "app.js"></script>
+    
+
+  </body>
+</html>
---
+++ includes/navigation.php
@@ -0,0 +1,13 @@
+<!-- Navigation Bar -->
+<nav class="navbar">
+      <div class="navbar_container"> <a href="/" id="navbar_logo">Charlene Mae Escarda</a> 
+        <div class="navbar_toggle" id="mobile-menu"> <span class="bar"></span><span class="bar"></span> <span class="bar"></span> </div>
+      </div>
+      <ul class="navbar_menu">
+        <li class="navbar_item"><a href="#" class="navbar_links">HOME</a></li>
+        <li class="navbar_item"><a href="#" class="navbar_links">ABOUT</a></li>
+        <li class="navbar_item"> <a href="#" class="navbar_links">PORTFOLIO</a> </li>
+        <li class="navbar_item"> <a href="#" class="navbar_links">SERVICE</a> </li>
+        <li class="navbar_item"> <a href="#" class="navbar_links">CONTACT</a> </li>
+      </ul>
+    </nav>
